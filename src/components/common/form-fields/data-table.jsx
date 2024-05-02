72a87187017efb81ef7614e46f33c6fd5d656402
ata324 <mavican324@gmail.com>
2024-05-02 15:31:01 +0200

    update

--- src/components/common/form-fields/data-table.jsx
+++ src/components/common/form-fields/data-table.jsx
@@ -1,42 +1,50 @@
-import React from "react";
+export const Column = ({ children }) => {
+  return <th>{children}</th>;
+};
 
+export const Cell = ({ children }) => {
+  return <td>{children}</td>;
+};
 
-
-export const Column = () => {
-  return (
-    <div>Column</div>
-  )
-}
-
+export const Row = ({ children }) => {
+  return <tr>{children}</tr>;
+};
 
 const DataTable = (props) => {
+  const { title, children, dataSource, dataKey } = props;
 
-    const { title, children } = props;
+  console.log(children);
 
-    console.log(children)
+  if (!dataSource || !Array.isArray(dataSource))
+    throw new Error("dataSources attribute is required");
 
-	return (
-		<div className="card">
-			<div className="card-body">
-                <h3 className="card-title">{title}</h3>
-				<div className="table-responsive">
-					<table className="table table-striped">
-						<thead>
-							<tr>
-								
-							</tr>
-						</thead>
-						<tbody>
-							<tr>
-								<td>sdfs</td>
-								<td>sdfssdd</td>
-							</tr>
-						</tbody>
-					</table>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="card">
+      <div className="card-body">
+        <h3 className="card-title">{title}</h3>
+        <div className="table-responsive">
+          <table className="table table-striped">
+            <thead>
+              <tr>{children}</tr>
+            </thead>
+            <tbody>
+              {dataSource.map((row) => (
+                <Row key={row[dataKey]}>
+                  {children.map((cell) => {
+                    const { dataField, index } = cell.props;
+                    let cellData = "";
+                    cellData = row[dataField];
+                    const cellKey=row[dataKey]+dataField+cellData;
+                    return <Cell key={cellKey}>{cellData}</Cell>;
+                  })}
+                </Row>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default DataTable;
--- src/components/dashboard/admin/admin-list.jsx
+++ src/components/dashboard/admin/admin-list.jsx
@@ -4,19 +4,25 @@ import React from "react";
 import { Container } from "react-bootstrap";
 
 const AdminList = () => {
-	return (
-		<Container>
-			<Link href="/dashboard/admin/new" className="btn btn-primary mb-3">
-				New
-			</Link>
+  const arr = [
+    { id: 2, name: "can", surname: "kan", username:"bal" },
+    { id: 3, name: "van", surname: "dam", username:"bal"},
+    { id: 4, name: "lan", surname: "bak", username:"bal"},
+  ];
 
-			<DataTable title="Admin List">
-                <Column title="First Name"/>
-                <Column title="Last Name"/>
-                <Column title="Username"/>
-            </DataTable>
-		</Container>
-	);
+  return (
+    <Container>
+      <Link href="/dashboard/admin/new" className="btn btn-primary mb-3">
+        New
+      </Link>
+
+      <DataTable title="Admin List" dataSource={arr} dataKey="id">
+        <Column dataField="name">First name</Column>
+        <Column dataField="surname">Last name</Column>
+        <Column dataField="username">Username</Column>
+      </DataTable>
+    </Container>
+  );
 };
 
 export default AdminList;
